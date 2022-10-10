43da347132255c55050f025779cfd56aef163356
AM4772 <moro_bramanti@hotmail.com>
2022-10-10 18:10:36 -0300

    portfolio update

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
+
+var encEmail = "bW9yb19icmFtYW50aUBob3RtYWlsLmNvbQ==";
+const form = document.getElementById("contact");
+form.setAttribute("href", "mailto:".concat(atob(encEmail)));
